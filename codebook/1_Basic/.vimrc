4b4567a669b87b39780c85eebfd41432148d747b
cy-cheng <cycheng.dev@gmail.com>
2024-09-27 23:53:29 +0800

    modify .vimrc (-DLOCAL) and default code

--- codebook/1_Basic/.vimrc
+++ codebook/1_Basic/.vimrc
@@ -3,5 +3,5 @@ set ru nu cin cul sc so=3 ts=4 sw=4 bs=2 ls=2 mouse=a
 ino {<CR> {<CR>}<C-o>O
 ino jj <esc>
 ino jk <esc> 
-map <F7> :w<CR>:!g++ "%" -std=c++17 -Wall -Wextra -Wshadow -Wconversion -fsanitize=address,undefined -g && ./a.out<CR>
+map <F7> :w<CR>:!g++ "%" -std=c++17 -DLOCAL -Wall -Wextra -Wshadow -Wconversion -fsanitize=address,undefined -g && ./a.out<CR>
 ca Hash w !cpp -dD -P -fpreprocessed \| tr -d "[:space:]" \| md5sum \| cut -c-6
--- codebook/1_Basic/default.cpp
+++ codebook/1_Basic/default.cpp
@@ -1,20 +1,36 @@
-// basic define and includes
 #include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
 #define pii pair<int, int>
 #define pll pair<ll, ll>
 #define pdd pair<double, double>
 #define ll long long
 #define ld long double
 #define i128 __int128
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-using namespace std;
+
+#ifdef LOCAL
+#define px(args...) LKJ("\033[1;32m(" #args "):\033[0m", args)
+template<class I> void LKJ(I&&x){ cerr << x << '\n'; }
+template<class I, class...T> void LKJ(I&&x, T&&...t){ cerr << x << ' ', LKJ(t...); }
+template<class I> void OI(I a, I b){ while(a < b) cerr << *a << " \n"[next(a) == b], ++a; }
+#define pv(v) cerr << "\033[1;31m" << #v << ": \033[0m"; OI(all(v))
+#else
+#define px(...) 
+#define OI(...) 
+#define pv(v)
+#endif
+
+template<class A, class B> ostream& operator<<(ostream &os, pair<A, B> p) { return os << '(' << p.F << ", " << p.S << ')'; }
 
 void solve() {}
 
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  solve();
+  cin.tie(0)->sync_with_stdio(0);
+  int T = 1;
+
+  // cin >> T;
+  while (T--) solve();
 }
